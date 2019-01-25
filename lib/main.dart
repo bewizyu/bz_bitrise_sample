5228e123571824958bc9d135f15f2d711bb576bb
Nicolas Hodicq <nartawak@gmail.com>
2019-01-25 10:23:56 +0100

    Update component and test

--- lib/main.dart
+++ lib/main.dart
@@ -6,36 +6,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Bitrise Demo'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'This app does nothing, Bewizyu Bitrise Demo',
-            ),
-          ],
+        title: 'nk_bitrise_sample',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-    );
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Bitrise Flutter"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'This app does nothing, Bewizyu Bitrise Demo',
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }

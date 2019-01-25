4aac9c12074df7ea65be0b0c444033b577313e79
Nicolas Hodicq <nartawak@gmail.com>
2019-01-25 10:23:56 +0100

    Update README

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # bz_bitrise_sample
 
-A new Flutter project.
+This project is a demo Flutter application with 3 favors (**dev**, **staging** and **prod**)
+to test the integration with Bitrise.
 
-## Getting Started
+You can read the related blog articles:
 
-This project is a starting point for a Flutter application.
+* [[Part 1] How to setup a Flutter application with Bitrise ?]()
+* Part 2 :point_right work in progress
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+## Step by step
+
+Branch|Compare view
+----|----
+init|[Starting point](https://github.com/bewizyu/bz_bitrise_sample/tree/init)
+flavor-android|[Compare flavor-android with init](https://github.com/bewizyu/bz_bitrise_sample/compare/init...flavor-android)
+flavor-ios|[Compare flavor-android with flavor-ios](https://github.com/bewizyu/bz_bitrise_sample/compare/flavor-android...flavor-ios)
+flavor-dart|[Compare flavor-ios with flavor-dart](https://github.com/bewizyu/bz_bitrise_sample/compare/flavor-ios...flavor-dart)
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,17 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:bz_bitrise_sample/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bz_bitrise_sample/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  group('MyApp', () {
+    testWidgets('display title', (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+      expect(find.text('Bitrise Flutter'), findsOneWidget);
+    });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    testWidgets('display content', (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+      expect(find.text('This app does nothing, Bewizyu Bitrise Demo'),
+          findsOneWidget);
+    });
   });
 }
